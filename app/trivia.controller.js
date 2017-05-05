e8af4edad4e9871b9b0c0778941eee6de98f4cb4
Collin McCoy <collinmccoy17@gmail.com>
2017-05-05 15:11:04 -0700

    question displays

--- app/trivia.controller.js
+++ app/trivia.controller.js
@@ -1,37 +1,56 @@
-(function() {
-    'use strict';
+(function () {
+  'use strict';
 
-   angular
-      .module('app')
-      .controller('TriviaController', TriviaController);
+  angular
+    .module('app')
+    .controller('TriviaController', TriviaController);
 
-   TriviaController.$inject = ['QuestionFactory'];
+  TriviaController.$inject = ['QuestionFactory'];
 
-   /* @ngInject */
-    function TriviaController(QuestionFactory) {
-      var triviaCtrl = this;
-      triviaCtrl.questions = "";
-      triviaCtrl.youtubeID = "";
-      triviaCtrl.results=[];
-      triviaCtrl.results.question="";
+  /* @ngInject */
+  function TriviaController(QuestionFactory) {
+    var triviaCtrl = this;
+    triviaCtrl.questionList = [];
+    triviaCtrl.youtubeID = "";
+    triviaCtrl.results = [];
+    triviaCtrl.currentIndex = 0;
+    // triviaCtrl.results.question = "";
+    // triviaCtrl.results.correct_answer = "";
+    
 
-     triviaCtrl.retrieve = function() {
+    triviaCtrl.next = function () {
+      if (triviaCtrl.currentIndex === triviaCtrl.results.length) {
+        triviaCtrl.currentIndex = 0;
+      } else {
+        triviaCtrl.currentIndex++;
+      }
+    }
+    triviaCtrl.back = function () {
+      if (triviaCtrl.currentIndex === triviaCtrl.results.length) {
+        triviaCtrl.currentIndex = 0
+      } else {
+        triviaCtrl.currentIndex--;
+      }
+    }
+
+
+    triviaCtrl.retrieve = function () {
         QuestionFactory
           .questionCreate()
-          .then(function(data) {
-              triviaCtrl.results = data;
-             
-           });
+          .then(function (data) {
+            triviaCtrl.results = data.results;
+      });
+    }
+triviaCtrl.questionPop= function () {
+         
+      triviaCtrl.currentQuestion = {
+        question: decodeURIComponent(triviaCtrl.results[triviaCtrl.currentIndex].question),
+        CorrectAnswer: triviaCtrl.results[triviaCtrl.currentIndex].correct_answer
+      };
 
-         } , 
 
-    triviaCtrl.searchYou = function() {
-       QuestionFactory
-         .playVideo()
-         .then(function(response){
+          };
 
-         });
-    
-  }
       }
-    })();
+
+})();
