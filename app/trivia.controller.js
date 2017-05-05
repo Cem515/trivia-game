5eda2732f07bc8ae0024e4252aa1664f60dc01a4
Collin McCoy <collinmccoy17@gmail.com>
2017-05-05 12:34:02 -0700

    added back

---
+++ app/trivia.controller.js
@@ -0,0 +1,37 @@
+(function() {
+    'use strict';
+
+   angular
+      .module('app')
+      .controller('TriviaController', TriviaController);
+
+   TriviaController.$inject = ['QuestionFactory'];
+
+   /* @ngInject */
+    function TriviaController(QuestionFactory) {
+      var triviaCtrl = this;
+      triviaCtrl.questions = "";
+      triviaCtrl.youtubeID = "";
+      triviaCtrl.results=[];
+
+     triviaCtrl.retrieve = function() {
+        QuestionFactory
+          .questionCreate()
+          .then(function(response) {
+              triviaCtrl.results = data;
+              console.log(data);
+
+           }
+
+         }
+
+    triviaCtrl.searchYou = function() {
+       QuestionFactory
+         .playVideo()
+         .then(function(response){
+
+         }
+    
+  }
+      }
+    })();
---
+++ app/trivia.factory.js
@@ -0,0 +1,65 @@
+(function() {
+  'use strict';
+
+ angular
+    .module('app')
+    .factory('QuestionFactory', QuestionFactory);
+
+ QuestionFactory.$inject = [''];
+
+ /* @ngInject */
+  function QuestionFactory() {
+    var service = {
+      questionCreate: questionCreate,
+      playVideo: playVideo
+    };
+
+   return service;
+
+   function questionCreate(question) {
+      return $http.get("https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&encode=url3986")
+        .then(function(response) {
+          return response.data;
+
+       });
+    }
+
+   function playVideo(video) {
+      var tag = document.createElement('script');
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+      var player;
+
+     function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+          height: '390',
+          width: '640',
+          videoId: 'M7lc1UVf-VE',
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+          }
+        });
+      }
+
+     function onPlayerReady(event) {
+        event.target.playVideo();
+      }
+      
+      var done = false;
+
+     function onPlayerStateChange(event) {
+        if (event.data == YT.PlayerState.PLAYING && !done) {
+          setTimeout(stopVideo, 6000);
+          done = true;
+        }
+      }
+
+     function stopVideo() {
+        player.stopVideo();
+      }
+    }
+
+ }
+})();
