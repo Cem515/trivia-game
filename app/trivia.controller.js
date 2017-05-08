0e6e118f590a5da78148705760638f8af1d7d44f
Collin McCoy <collinmccoy17@gmail.com>
2017-05-07 18:23:47 -0700

    weekend work

--- app/trivia.controller.js
+++ app/trivia.controller.js
@@ -14,9 +14,9 @@
     triviaCtrl.youtubeID = "";
     triviaCtrl.results = [];
     triviaCtrl.currentIndex = 0;
-    // triviaCtrl.results.question = "";
-    // triviaCtrl.results.correct_answer = "";
-    
+    triviaCtrl.beerResults = [];
+
+    //Next and Back Buttons
 
     triviaCtrl.next = function () {
       if (triviaCtrl.currentIndex === triviaCtrl.results.length) {
@@ -24,33 +24,64 @@
       } else {
         triviaCtrl.currentIndex++;
       }
-    }
+    };
+
     triviaCtrl.back = function () {
       if (triviaCtrl.currentIndex === triviaCtrl.results.length) {
         triviaCtrl.currentIndex = 0
       } else {
         triviaCtrl.currentIndex--;
       }
-    }
+    };
 
+    //Retrieve Questions from Database
 
     triviaCtrl.retrieve = function () {
-        QuestionFactory
-          .questionCreate()
-          .then(function (data) {
-            triviaCtrl.results = data.results;
-      });
+      QuestionFactory
+        .questionCreate()
+        .then(function (data) {
+          triviaCtrl.results = data.results;
+        });
     }
-triviaCtrl.questionPop= function () {
-         
+
+    //Populate Questions
+    triviaCtrl.questionPop = function () {
+
       triviaCtrl.currentQuestion = {
         question: decodeURIComponent(triviaCtrl.results[triviaCtrl.currentIndex].question),
-        CorrectAnswer: triviaCtrl.results[triviaCtrl.currentIndex].correct_answer
-      };
-
-
-          };
 
       }
+
+      triviaCtrl.answers = {
+
+        CorrectAnswer: decodeURIComponent(triviaCtrl.results[triviaCtrl.currentIndex].correct_answer),
+        IncorrectAnswer: decodeURIComponent(triviaCtrl.results[triviaCtrl.currentIndex].incorrect_answers)
+      }
+
+
+    };
+    //Gets Random Beer Information
+    triviaCtrl.beerInfo = function () {
+      QuestionFactory
+        .randomBeer()
+        .then(function (data) {
+          triviaCtrl.beerResults = data;
+        });
+
+      //Populate Beer Information
+
+      triviaCtrl.beerPop = function () {
+        triviaCtrl.beer = {
+          name: (triviaCtrl.beerResults[0].nameDisplay),
+          abv: (triviaCtrl.beerResults[0].abv),
+          style: (triviaCtrl.beerResults[0].shortName),
+          description: (triviaCtrl.beerResults[0].description),
+          // label: (triviaCtrl.beerResults.labels.large),
+        }
+
+      }
+    };
+
+  }
 
 })();
--- app/trivia.factory.js
+++ app/trivia.factory.js
@@ -1,65 +1,37 @@
-(function() {
+(function () {
   'use strict';
 
- angular
+  angular
     .module('app')
     .factory('QuestionFactory', QuestionFactory);
 
- QuestionFactory.$inject = ['$http'];
+  QuestionFactory.$inject = ['$http'];
 
- /* @ngInject */
+  /* @ngInject */
   function QuestionFactory($http) {
     var service = {
       questionCreate: questionCreate,
-      playVideo: playVideo
+      randomBeer: randomBeer
     };
 
-   return service;
+    return service;
 
-   function questionCreate(question) {
+    function questionCreate(question) {
       return $http.get("https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&encode=url3986")
-        .then(function(response) {
+        .then(function (response) {
           return response.data;
 
-       });
-    }
-
-   function playVideo(video) {
-      var tag = document.createElement('script');
-      tag.src = "https://www.youtube.com/iframe_api";
-      var firstScriptTag = document.getElementsByTagName('script')[0];
-      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-      var player;
-
-     function onYouTubeIframeAPIReady() {
-        player = new YT.Player('player', {
-          height: '390',
-          width: '640',
-          videoId: 'M7lc1UVf-VE',
-          events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-          }
         });
-      }
-
-     function onPlayerReady(event) {
-        event.target.playVideo();
-      }
-      
-      var done = false;
-
-     function onPlayerStateChange(event) {
-        if (event.data == YT.PlayerState.PLAYING && !done) {
-          setTimeout(stopVideo, 6000);
-          done = true;
-        }
-      }
-
-     function stopVideo() {
-        player.stopVideo();
-      }
     }
 
- }
+    function randomBeer(beer) {
+      return $http.get("http://api.brewerydb.com/v2/beer/random?key=cf772398dc53cc03dcd9241d3d9b3cff&format=json")
+        .then(function (response) {
+          return response.data
+
+        })
+
+    }
+
+  }
 })();
